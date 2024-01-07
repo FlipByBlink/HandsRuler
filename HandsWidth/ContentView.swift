15d11f4477e226d592cfbfca8fe779d72cba1047
aaaa <a@a>
2024-01-08 08:56:37 +0900

    Initial Commit

---
+++ HandsWidth/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  HandsWidth
+//
+//  Created by FULLNAME on 2024/01/08.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    @State var enlarge = false
+
+    var body: some View {
+        VStack {
+            RealityView { content in
+                // Add the initial RealityKit content
+                if let scene = try? await Entity(named: "Scene", in: realityKitContentBundle) {
+                    content.add(scene)
+                }
+            } update: { content in
+                // Update the RealityKit content when SwiftUI state changes
+                if let scene = content.entities.first {
+                    let uniformScale: Float = enlarge ? 1.4 : 1.0
+                    scene.transform.scale = [uniformScale, uniformScale, uniformScale]
+                }
+            }
+            .gesture(TapGesture().targetedToAnyEntity().onEnded { _ in
+                enlarge.toggle()
+            })
+
+            VStack {
+                Toggle("Enlarge RealityView Content", isOn: $enlarge)
+                    .toggleStyle(.button)
+            }.padding().glassBackgroundEffect()
+        }
+    }
+}
+
+#Preview(windowStyle: .volumetric) {
+    ContentView()
+}
---
+++ HandsWidth/HandsWidthApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandsWidthApp.swift
+//  HandsWidth
+//
+//  Created by FULLNAME on 2024/01/08.
+//
+
+import SwiftUI
+
+@main
+struct HandsWidthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
