32f7b6614810ef9da5746bba027875678f42f51f
aaaa <a@a>
2024-07-22 11:42:47 +0900

    fix "00 ft 12 ft" bug.

--- HandsRuler/Rest/🧾ResultFormatter.swift
+++ HandsRuler/Rest/🧾ResultFormatter.swift
@@ -3,29 +3,32 @@ import Foundation
 enum 🧾ResultFormatter {
     static func string(_ lineLength: Float, _ unit: 📐Unit) -> String {
         if unit == .feetAndInches {
-            let measurement = Measurement(value: .init(lineLength), unit: UnitLength.meters)
-            let feetFormatter = MeasurementFormatter()
-            feetFormatter.unitOptions = .providedUnit
-            feetFormatter.numberFormatter.maximumFractionDigits = 0
-            let inchFormatter = MeasurementFormatter()
-            inchFormatter.unitOptions = .providedUnit
-            inchFormatter.numberFormatter.maximumFractionDigits = 0
-            inchFormatter.numberFormatter.minimumIntegerDigits = 2
-            let feetValue = measurement.converted(to: .feet).value
-            let rounded = feetValue.rounded(.towardZero)
-            let feetPart = Measurement(value: rounded, unit: UnitLength.feet)
-            let inchPart = Measurement(value: feetValue - rounded, unit: UnitLength.feet).converted(to: .inches)
-            return ("\(feetFormatter.string(from: feetPart)) \(inchFormatter.string(from: inchPart))")
+            let measurement = Measurement(value: .init(lineLength),
+                                          unit: UnitLength.meters)
+            
+            let inchValue = Int(measurement.converted(to: .inches).value)
+            
+            let feetFormatter = NumberFormatter()
+            feetFormatter.maximumFractionDigits = 0
+            let feetPart = "\(feetFormatter.string(for: inchValue / 12) ?? "-") ft"
+            
+            let inchFormatter = NumberFormatter()
+            inchFormatter.maximumFractionDigits = 0
+            inchFormatter.minimumIntegerDigits = 2
+            let inchPart = "\(inchFormatter.string(for: inchValue % 12) ?? "--") in"
+            
+            return ("\(feetPart) \(inchPart)")
         } else {
             let formatter = MeasurementFormatter()
             formatter.unitOptions = .providedUnit
-            var fractionDigits: Int {
+            let fractionDigits: Int = {
                 switch unit {
-                    case .centiMeters, .inches: 1
+                    case .centiMeters: 0
+                    case .inches: 1
                     case .meters, .feet, .yards: 2
                     case .feetAndInches: fatalError()
                 }
-            }
+            }()
             formatter.numberFormatter.minimumFractionDigits = fractionDigits
             formatter.numberFormatter.maximumFractionDigits = fractionDigits
             let measurement = Measurement(value: .init(lineLength),
